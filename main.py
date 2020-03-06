af22d3620aad7fd50afca3d0b73b697b6819a3b6
Alex Liddle <aliddle@rsitex.com>
2020-03-06 07:50:05 -0600

    added some data cleaning and transformations

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 def main():
-    print('hello world!')
+    msg = 'hello world!'
+
+    vowels = ['a','e','i','o','u']
+
+    msg = list(msg)
+    msg_copy = msg.copy()
+    idx = 0
+    for i in range(len(msg)):
+        if msg[i] in vowels:
+            msg_copy.pop(idx)
+            idx -= 1
+        idx += 1
+
+    msg = ''.join(msg_copy)
+    msg = msg.upper()
+
+    print(msg)
+    
 
 main()
